a620bac02adca933a704ee222d102b2e331a9fef
michelefagundes <michelefagundes.santos@gmail.com>
2025-09-03 06:47:47 -0300

    adicionando os seguintes testes: transferencia com sucesso, sem saldo para transferencia, token não informado

--- test/external/transferExternal.test.js
+++ test/external/transferExternal.test.js
@@ -62,5 +62,48 @@ describe('Transfer', () => {
             delete respostaEsperada.date; 
             expect(resposta.body).to.deep.equal(respostaEsperada);
         });
+
+        it('Transferência com sucesso', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    from: "julio",
+                    to: "priscila",
+                    value: 50
+                });
+
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body).to.have.property('from', 'julio');
+            expect(resposta.body).to.have.property('to', 'priscila');
+            expect(resposta.body).to.have.property('value', 50);
+        });
+
+        it('Sem saldo disponível para transferência', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    from: "julio",
+                    to: "priscila",
+                    value: 85000000000
+                });
+            
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Saldo insuficiente');
+        });
+
+        it('Token de autenticação não informado', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/transfers')
+                .send({
+                    from: "julio",
+                    to: "priscila",
+                    value: 50
+                });
+            
+            expect(resposta.status).to.equal(401);
+            expect(resposta.body).to.have.property('message', 'Token não fornecido.');
+        });
     });
 });
